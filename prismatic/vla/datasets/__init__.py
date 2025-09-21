ebfe9ec481ef13bf026f689c4027bb15570b255c
WangYH <93377669+WangYH-BUPT@users.noreply.github.com>
2025-09-22 01:59:41 +0800

    Update __init__.py

--- prismatic/vla/datasets/__init__.py
+++ prismatic/vla/datasets/__init__.py
@@ -1,1 +1,1 @@
-from .datasets import DummyDataset, EpisodicRLDSDataset, RLDSBatchTransform, RLDSDataset, RLDSBatchTransform_V1
+from .datasets import DummyDataset, EpisodicRLDSDataset, RLDSBatchTransform, RLDSDataset
