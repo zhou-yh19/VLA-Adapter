aba1a49b8b58e23cc9540556f469a7a20c111e7a
WangYH <93377669+WangYH-BUPT@users.noreply.github.com>
2025-09-22 01:42:42 +0800

    Update constants.py

--- prismatic/vla/constants.py
+++ prismatic/vla/constants.py
@@ -32,6 +32,13 @@ LIBERO_CONSTANTS = {
     "ACTION_PROPRIO_NORMALIZATION_TYPE": NormalizationType.BOUNDS_Q99,
 }
 
+CALVIN_CONSTANTS = {
+    "NUM_ACTIONS_CHUNK": 8,
+    "ACTION_DIM": 7,
+    "PROPRIO_DIM": 8,
+    "ACTION_PROPRIO_NORMALIZATION_TYPE": NormalizationType.BOUNDS_Q99,
+}
+
 ALOHA_CONSTANTS = {
     "NUM_ACTIONS_CHUNK": 25,
     "ACTION_DIM": 14,
@@ -57,6 +64,8 @@ def detect_robot_platform():
         return "ALOHA"
     elif "bridge" in cmd_args:
         return "BRIDGE"
+    elif "calvin" in cmd_args:
+        return "CALVIN"
     else:
         # Default to LIBERO if unclear
         return "LIBERO"
@@ -72,6 +81,8 @@ elif ROBOT_PLATFORM == "ALOHA":
     constants = ALOHA_CONSTANTS
 elif ROBOT_PLATFORM == "BRIDGE":
     constants = BRIDGE_CONSTANTS
+elif ROBOT_PLATFORM == "CALVIN":
+    constants = CALVIN_CONSTANTS
 
 # Assign constants to global variables
 NUM_ACTIONS_CHUNK = constants["NUM_ACTIONS_CHUNK"]
